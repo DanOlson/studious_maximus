05e1379aa2e63ef7deb7c55108644f42ba25b030
DanOlson <815279+DanOlson@users.noreply.github.com>
2026-09-16 23:07:05 +0000

    Implement phase 0 Rust foundation

--- crates/app/src/lms.rs
+++ crates/app/src/lms.rs
@@ -1,3 +1,4 @@
+#[cfg(feature = "write")]
 pub mod canvas;
 pub mod dto;
 
--- crates/app/src/support.rs
+++ crates/app/src/support.rs
@@ -5,14 +5,14 @@ use sqlx::SqlitePool;
 use crate::models::{Assignment, RawDbSubmission, Submission};
 
 pub async fn all_assignments(pool: &SqlitePool) -> Vec<Assignment> {
-    sqlx::query_as!(Assignment, "select * from assignments order by id asc")
+    sqlx::query_as::<_, Assignment>("select * from assignments order by id asc")
         .fetch_all(pool)
         .await
         .unwrap()
 }
 
 pub async fn all_submissions(pool: &SqlitePool) -> Vec<Submission> {
-    sqlx::query_as!(RawDbSubmission, "select * from submissions order by id asc")
+    sqlx::query_as::<_, RawDbSubmission>("select * from submissions order by id asc")
         .fetch_all(pool)
         .await
         .unwrap()
